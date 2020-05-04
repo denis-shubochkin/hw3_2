62851eb6075c5e3dee6bde4e0df4cc17b2b040ab
Denis.Shubochkin <zippo1095@gmail.com>
2020-05-04 16:51:48 +0300

    hw3_2_lint

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -2,14 +2,14 @@ import order from '../basic';
 
 test('ordered massives not equaled with toBe', () => {
   const p = [
-    {name: 'мечник', health: 10},
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
   ];
   const exp = [
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
-    {name: 'мечник', health: 10},
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
   ];
   const result = order(p);
   expect(result).not.toBe(exp);
@@ -17,14 +17,14 @@ test('ordered massives not equaled with toBe', () => {
 
 test('ordered massives equaled with toEqual', () => {
   const p = [
-    {name: 'мечник', health: 10},
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
   ];
   const exp = [
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
-    {name: 'мечник', health: 10},
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
   ];
   const result = order(p);
   expect(result).toEqual(exp);
--- src/js/basic.js
+++ src/js/basic.js
@@ -1,4 +1,4 @@
 export default function order(players) {
- players.sort((prev,next) => next.health - prev.health);
- return players;
+  players.sort((prev, next) => next.health - prev.health);
+  return players;
 }
